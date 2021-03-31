b3a9a5087964567a60b87fd379a701b4a937eae7
shubham <shubham.ingole@neosofttech.com>
2021-03-31 18:09:53 +0530

    --all

---
+++ src/Card.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./Card.css";
+
+function Card({ src, title, description, price }) {
+  console.log("in card");
+  return (
+    <div className="card">
+      <img src={src} alt="" />
+      <div className="card__info">
+        <h2>{title}</h2>
+        <h4>{description}</h4>
+        <h3>{price}</h3>
+      </div>
+    </div>
+  );
+}
+
+export default Card;
---
+++ src/Header.js
@@ -0,0 +1,31 @@
+import React from "react";
+import "./Header.css";
+import logo from "./airbnb.png";
+import SearchIcon from "@material-ui/icons/Search";
+import LanguageIcon from "@material-ui/icons/Language";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import { Avatar } from "@material-ui/core";
+import { Link } from "react-router-dom";
+
+function Header() {
+  return (
+    <div className="header">
+      <Link to="/">
+        <img className="header__icon" src={logo} />
+      </Link>
+
+      <div className="header__center">
+        <input type="text" />
+        <SearchIcon />
+      </div>
+      <div className="header_right">
+        <p>Become a host</p>
+        <LanguageIcon />
+        <ExpandMoreIcon />
+        <Avatar />
+      </div>
+    </div>
+  );
+}
+
+export default Header;
